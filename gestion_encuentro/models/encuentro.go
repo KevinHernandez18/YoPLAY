ccad49fb8b3e3da008bb04324245cd9ce681d9e3
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-04-28 13:52:28 -0500

    Se agrega el modelo encuentro, se predefinen rutas y se crea funcion respondJSON

--- gestion_encuentro/models/encuentro.go
+++ gestion_encuentro/models/encuentro.go
@@ -1,1 +1,18 @@
 package models
+
+import "time"
+
+type Encuentro struct {
+	Id_Encuentro         int `json: "id_encuentro"`
+	Fecha                time.Time `json: "fecha"`
+	Id_Torneo            int `json: "id_torneo"`
+	Id_tipo_distribucion int `json: "id_tipo_distribucion"`
+	Fase_torneo 		 string `json: "fase_torneo"`
+	Id_Equipo1			 int `json: "id_Equipo1"`
+	Resultado_Equipo1 	 string `json: "resultado equipo1"`
+	Id_Equipo2 			 int `json: "id_equipo2"`
+	Resultado_Equipo2 	 string `json: "resultado_equipo2"`
+	Activo 				 bool `json: "activo"`
+	Fecha_Creacion 		 time.Time `json: fecha_creacion`
+	Fecha_Modificacion 	 time.Time `json: "fecha_modificacion"`
+}
--- gestion_encuentro/routes/encuentro_routes.go
+++ gestion_encuentro/routes/encuentro_routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import(
+	"API_GO_CRUD/controllers"
+	
+	"github.com/gorilla/mux"
+)
+
+func RegisterCredentialRoutes(r *mux.Router){
+	r.HandleFunc("/encuentro", controllers.GetALLEncuentros).Methods("GET")
+	r.HandleFunc("/encuentro/{id}", controllers.GetEncuentroByID).Methods("GET")
+	r.HandleFunc("/encuentro", controllers.CreateEncuentro).Methods("POST")
+	r.HandleFunc("/encuentro/{id}", controllers.UpdateEncuentro).Methods("PUT")
+	r.HandleFunc("/encuentro/{id}", controllers.DeleteEncuentro).Methods("DELETE")
+}
