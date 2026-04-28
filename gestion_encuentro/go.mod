852fb43fa030c63058ebcc3b53adbca11d02f90e
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-04-28 13:28:25 -0500

    Se diseña la estrura base del desarrollo de las API desde Main y DB.go

---
+++ gestion_encuentro/go.mod
@@ -0,0 +1,5 @@
+module gestion_encuentro
+
+go 1.23.1
+
+require github.com/lib/pq v1.12.3
--- gestion_encuentro/main.go
+++ gestion_encuentro/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"API_GO_CRUD/config"
+	"API_GO_CRUD/routes"
+
+	"github.com/gorilla/mux"
+)
+
+// middleware CORS
+func enableCORS(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		// Permiten cualquier origen de la petición
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		
+		if r.Method == "OPTIONS" {
+			return
+		}
+		
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main(){
+	config.ConnectDB() // Conexión a la base de datos
+
+	r := mux.NewRouter()
+
+	// Registro de rutas
+	//routes.
+	//routes.
+	//routes.
+	//routes.
+	//routes.
+
+	log.Println("Servidor operando en el puerto 8090")
+
+	log.Fatal(http.ListenAndServe(":8090", enableCORS(r)))
+}
