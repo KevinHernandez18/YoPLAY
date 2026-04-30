5923726f8df253e58813abdd964f37b10d97d802
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-04-30 15:45:49 -0500

    Se integra función UpdateGrupoEncuentro para el esquema Gestion_Encuentro, se actualizan funciones para la función GetALLGrupoEncuentro

--- gestion_encuentro/controllers/grupo_encuentro_controllers.go
+++ gestion_encuentro/controllers/grupo_encuentro_controllers.go
@@ -11,8 +11,8 @@ import (
 	//"github.com/gorilla/mux" //Librería para crear rutas
 )
 
-func GetAllGrupoencuentro(w http.ResponseWriter, r *http.Request) {
-	rows, err := config.DB.Query("SELECT id_grupo_encuentro, id_grupo, id_encuentro, activo FROM grupo_encuentro")
+func GetAllGrupoEncuentro(w http.ResponseWriter, r *http.Request) {
+	rows, err := config.DB.Query("SELECT id_grupo_encuentro, id_grupo, id_encuentro, activo, fecha_creacion, fecha_modificacion FROM grupo_encuentro")
 
 	if err != nil {
 		respondJSON(w, 500, map[string]string{"Error:": err.Error()})
@@ -24,7 +24,7 @@ func GetAllGrupoencuentro(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		var GE models.Grupo_Encuentro
-		rows.Scan(&GE.Id_Grupo_Encuentro, &GE.Id_Grupo, &GE.Id_Encuentro, &GE.Activo)
+		rows.Scan(&GE.Id_Grupo_Encuentro, &GE.Id_Grupo, &GE.Id_Encuentro, &GE.Activo, &GE.Fecha_Creacion, &GE.Fecha_Modificacion)
 		list = append(list, GE)
 	}
 
@@ -63,4 +63,22 @@ func CreateGrupoEncuentro(w http.ResponseWriter, r *http.Request){
 	}
 
 	respondJSON(w, 201, GE)
+}
+
+func UpdateGrupoEncuentro(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	var GE models.Grupo_Encuentro
+	json.NewDecoder(r.Body).Decode(&GE)
+
+	_, err := config.DB.Exec(
+		"UPDATE grupo_encuentro SET id_grupo=$1, id_encuentro=$2, activo=$3, fecha_modificacion=now() WHERE id_grupo_encuentro=$4",
+		GE.Id_Grupo, GE.Id_Encuentro, GE.Activo, id,
+	)
+
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"Error:": err.Error()})
+		return
+	}
+	respondJSON(w, 200, map[string]string{"Message": "Dato Actualizado"})
 }
--- gestion_encuentro/routes/grupo_encuentro_routes.go
+++ gestion_encuentro/routes/grupo_encuentro_routes.go
@@ -7,9 +7,9 @@ import(
 )
 
 func RegisterGrupoEncuentroRoutes(r *mux.Router){
-	r.HandleFunc("/grupoencuentro", controllers.GetAllGrupoencuentro).Methods("GET")
+	r.HandleFunc("/grupoencuentro", controllers.GetAllGrupoEncuentro).Methods("GET")
 	r.HandleFunc("/grupoencuentro/{id}", controllers.GetGrupoEncuentroByID).Methods("GET")
 	r.HandleFunc("/grupoencuentro", controllers.CreateGrupoEncuentro).Methods("POST")
-	//r.HandleFunc("/grupoencuentro/{id}", controllers.UpdateGrupoEncuentro).Methods("PUT")
+	r.HandleFunc("/grupoencuentro/{id}", controllers.UpdateGrupoEncuentro).Methods("PUT")
 	//r.HandleFunc("/grupoencuentro/{id}", controllers.DeleteGrupoEncuentro).Methods("DELETE")
 }
